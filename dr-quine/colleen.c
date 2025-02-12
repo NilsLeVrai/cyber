0f6ea3a9567cd0f1f4526a9d3cbdb7a6d7ccb622
NilsLeVrai <nilslucas@wanadoo.fr>
2025-02-12 17:40:53 +0100

    basique non conforme, marche qu'avec une fonction main

--- dr-quine/colleen.c
+++ dr-quine/colleen.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/*
+Djibril Cisse
+*/
+
 int main()
 {
 	char	*s;
 
-	s = "#include <stdio.h>%c%cint main()%c{%c%cchar%c*s;%c%c%cs = %c%s%c;%c%cprintf(s, 10, 10, 10, 10, 9, 9, 10, 10, 9, 34, s, 34, 10, 9, 10);%c}";
-	printf(s, 10, 10, 10, 10, 9, 9, 10, 10, 9, 34, s, 34, 10, 9, 10);
+	s = "#include <stdio.h>%c%c/*%cDjibril Cisse%c*/%c%cint main()%c{%c%cchar%c*s;%c%c%cs = %c%s%c;%c%cprintf(s, 10, 10, 10, 10, 10, 10, 10, 10, 9, 9, 10, 10, 9, 34, s, 34, 10, 9, 10, 9, 10);%c%c//Sidney Govou%c}";
+	printf(s, 10, 10, 10, 10, 10, 10, 10, 10, 9, 9, 10, 10, 9, 34, s, 34, 10, 9, 10, 9, 10);
+	//Sidney Govou
 }
