726deca9a83d64420f12adb92d8b12f3b2dbdd47
NilsLeVrai <nilslucas@wanadoo.fr>
2026-05-27 16:57:55 +0200

    Colleen asm not working

--- dr-quine/ASM/Colleen.s
+++ dr-quine/ASM/Colleen.s
@@ -1,17 +1,26 @@
-global _start
+global main
 
-section .data
-	message: db "Hello", 10
+extern printf
+extern exit
+; outside comment
 
 section .text
-_start:
-	mov rdi, 1
-	mov rsi, message
-	mov rax, 1
-	mov rdx, 6
-	syscall
+	main:
+		push rbp
+		mov rbp, rsp
 
-	mov rax, 60
-	mov rdi, 0
-	syscall
+		; add arguments
+		lea rdi, [str]
+		mov rsi, 10
+		mov rdx, 34
+		mov rcx, 9
+		lea r8, [rel]
+		call printf
+		xor rdi, rdi
+		call exit
 
+func:
+	ret
+
+section .rodata
+	str: db 'global main%1$c%1$cextern printf%1$cextern exit%1$c; outside comment%1$c%1$csection .text%1$c%3$cmain:%1$c%3$c%3$cpush rbp%1$c%3$c%3$cmov rbp, rsp%1$c%1$c%3$c%3$c; add arguments%1$c%3$c%3$cmov rsi, 10%1$c%3$c%3$cmov rdx, 34%1$c%3$c%3$cmov rcx, 9%1$c%3$c%3$clea rdi, [str]%1$c%3$c%3$ccall printf%1$c%3$c%3$ccall exit%1$c%1$cfunc:%1$c%3$cret%1$c%1$csection .data:%1$c%3$c', 0xa, 0
